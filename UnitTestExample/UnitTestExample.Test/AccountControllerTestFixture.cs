694df3647883fb7ca297c0c4f737ac7944a4ac0d
o39nq1 <andras.hortobagyi@stud.uni-corvinus.hu>
2024-04-21 17:48:20 +0200

    uj nuget csomagok telepítése

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UnitTestExample.Test
+{
+    using NUnit.Framework;
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // ...
+        }
+    }
+}
